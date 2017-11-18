bc17e7b98f6c216c2ea1cdc9c8142f36c8afe4f1
Guido van Rossum <guido@dropbox.com>
2017-11-18 15:31:08 -0800

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pyannotate',
-      version='1.0.0',
+      version='1.0.1',
       description="PyAnnotate: Auto-generate PEP-484 annotations",
       author='Dropbox',
       author_email='guido@dropbox.com',
